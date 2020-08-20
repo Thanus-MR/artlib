ffb7ac217d5ab920e8da449687a1b02f22c0df6b
Sri Lakshmi Kanthan P <srilakshmikanthan.p@gmail.com>
2020-08-20 17:39:07 +0530

    removed doc files from track

--- artlib - c++/doc/html/search/files_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['anixt_5fconfig_2ehpp_115',['anixt_config.hpp',['../anixt__config_8hpp.html',1,'']]],
-  ['anixt_5ffont_2ehpp_116',['anixt_font.hpp',['../anixt__font_8hpp.html',1,'']]],
-  ['anixt_5fstyle_2ehpp_117',['anixt_style.hpp',['../anixt__style_8hpp.html',1,'']]],
-  ['artlib_2ehpp_118',['artlib.hpp',['../artlib_8hpp.html',1,'']]]
-];
--- artlib - c++/doc/html/search/files_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['basic_5fanixt_2ehpp_119',['basic_anixt.hpp',['../basic__anixt_8hpp.html',1,'']]],
-  ['basic_5fruneape_2ehpp_120',['basic_runeape.hpp',['../basic__runeape_8hpp.html',1,'']]]
-];
