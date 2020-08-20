3cc64f73967a0ece00642ab341cc86df1e366ea1
Sri Lakshmi Kanthan P <srilakshmikanthan.p@gmail.com>
2020-08-20 18:35:04 +0530

    doc

--- Doxygen/html/search/typedefs_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['size_5ftype_193',['size_type',['../structsri_1_1art_1_1anixt__config.html#acdaecd070aa806637514d4e4d7abf05b',1,'sri::art::anixt_config::size_type()'],['../structsri_1_1art_1_1runeape__config.html#a05dc9e305954a26f6e44b0c46b2f34fc',1,'sri::art::runeape_config::size_type()']]],
-  ['string_5fbuff_5ftype_194',['string_buff_type',['../classsri_1_1art_1_1basic__anixt.html#ad54eb5486439355d1be001b5f8fda6a0',1,'sri::art::basic_anixt']]],
-  ['string_5ftype_195',['string_type',['../structsri_1_1art_1_1anixt__config.html#ab63d8db4c4694f6b8a56475b82e1aa59',1,'sri::art::anixt_config::string_type()'],['../structsri_1_1art_1_1anixt__config_1_1anixt__letter.html#aee457cefc3cfec45e8c094f637f37d62',1,'sri::art::anixt_config::anixt_letter::string_type()'],['../structsri_1_1art_1_1anixt__config_1_1anixt__string.html#afc88f693b7667651216062e404d178ef',1,'sri::art::anixt_config::anixt_string::string_type()'],['../structsri_1_1art_1_1runeape__config.html#adca82d7ac928c91ff1b706ee941a2435',1,'sri::art::runeape_config::string_type()'],['../structsri_1_1art_1_1runeape__config_1_1runeape__image.html#a532502af3dbf77764f7b13d8b5adc81d',1,'sri::art::runeape_config::runeape_image::string_type()']]],
-  ['style_5ftype_196',['style_type',['../classsri_1_1art_1_1basic__anixt.html#abff0c5c7fab5813fedf2372073c7ce22',1,'sri::art::basic_anixt::style_type()'],['../classsri_1_1art_1_1basic__runeape.html#a83556badb66764830c7a29d27fd88b95',1,'sri::art::basic_runeape::style_type()']]]
-];
