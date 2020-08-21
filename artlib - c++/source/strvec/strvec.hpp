9675ff4d825a16b3ebaf54d55f3b0ea211dc892f
Sri Lakshmi Kanthan P <srilakshmikanthan.p@gmail.com>
2020-08-21 12:05:44 +0530

    added strvec

---
+++ artlib - c++/source/strvec/strvec.hpp
@@ -0,0 +1,87 @@
+#ifndef STRVEC
+#define STRVEC
+
+#include <string>
+#include <vector>
+
+/**
+ * @namespace sri
+ * @brief outer namespace
+ **/
+namespace sri
+{
+    /**
+     * @namespace art
+     * @brief contains art classes and functions
+     **/
+    namespace art
+    {
+        /**
+         * @struct strvec
+         * @brief  vector of string
+         * @tparam String string_type
+         * @tparam Alloc allocato type
+         **/
+        template <typename String, template <typename> typename Alloc>
+        struct strvec : private std::vector<String, Alloc<String>>
+        {
+            using base        = std::vector<String, Alloc<String>>;
+            using string_type = String;
+            template <typename T>
+            using alloc_type  = Alloc<T>;
+
+            using typename base::allocator_type;
+            using typename base::const_iterator;
+            using typename base::const_pointer;
+            using typename base::const_reference;
+            using typename base::const_reverse_iterator;
+            using typename base::difference_type;
+            using typename base::iterator;
+            using typename base::pointer;
+            using typename base::reference;
+            using typename base::reverse_iterator;
+            using typename base::size_type;
+            using typename base::value_type;
+
+            using base::base;
+            virtual ~strvec() = default;
+
+            using base::operator=;
+            using base::assign;
+            using base::at;
+            using base::get_allocator;
+            using base::operator[];
+            using base::back;
+            using base::begin;
+            using base::capacity;
+            using base::cbegin;
+            using base::cend;
+            using base::clear;
+            using base::data;
+            using base::emplace;
+            using base::empty;
+            using base::end;
+            using base::erase;
+            using base::front;
+            using base::insert;
+            using base::max_size;
+            using base::push_back;
+            using base::rbegin;
+            using base::rend;
+            using base::reserve;
+            using base::resize;
+            using base::shrink_to_fit;
+            using base::size;
+            using base::swap;
+
+            friend void swap( strvec &lhs, strvec &rhs )
+            {
+                lhs.swap( rhs );
+            }
+        };
+
+    } // namespace art
+
+} // namespace sri
+
+#endif
